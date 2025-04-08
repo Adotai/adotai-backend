e8eb11de41f9dff15876222c52033ecf8e059f9b
JoaoCustodio66 <joaocustodiokintal@gmail.com>
2025-04-08 13:43:08 -0300

    Initial Spring Boot project

---
+++ src/main/java/com/adotai/backend_adotai/BackendAdotaiApplication.java
@@ -0,0 +1,13 @@
+package com.adotai.backend_adotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdotaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdotaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adotai/backend_adotai/BackendAdotaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adotai.backend_adotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdotaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
